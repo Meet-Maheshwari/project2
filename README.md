da5d3b43f3108baf13e13de123b8d65322cada9e
Meet-Maheshwari <meetmaheshwari2107@gmail.com>
2024-12-09 09:57:32 +0530

    Adding Backend

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This project was created from local system.
 Created by Meet Maheshwari
+
+I am trying to add some new features in this.
---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.listen(8080, () => {
+    console.log("app is listening on port 8080");
+});
+
+app.use("/", (req, res) => {
+    res.send("Hello, Welcome to my project2");
+})
